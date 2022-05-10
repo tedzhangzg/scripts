02313a8a3386d925868b5fc6b58fdcb6dee3acfc
tedzhangzg <83065920+tedzhangzg@users.noreply.github.com>
2022-05-10 10:19:39 +0800

    Create urls.sh

---
+++ urls.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Script
+# URL References
+
+# To download and include
+# wget -O - https://raw.githubusercontent.com/tedzhangzg/shellscripts/main/urls.sh | bash
+
+
+
+# Microsoft Edge
+url_microsoft_edge="https://go.microsoft.com/fwlink/?linkid=2069148"
+
+# Microsoft Teams
+url_microsoft_teams="https://aka.ms/teamsmac"
+
+# Microsoft Skype
+url_skype="https://go.skype.com/mac.download"
+
+# Google Chrome
+url_google_chrome="https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
+url_google_chrome_pkg="https://dl.google.com/dl/chrome/mac/universal/stable/gcem/GoogleChrome.pkg"
+
+# Google Drive
+url_google_drive="https://dl.google.com/drive-file-stream/GoogleDrive.dmg"
+
+# TeamViewer QuickSupport v13
+url_teamviewer="https://download.teamviewer.com/download/version_13x/TeamViewerQS.dmg"
+
+# Java 8 Runtime
+url_java8="https://javadl.oracle.com/webapps/download/GetFile/1.8.0_331-b09/165374ff4ea84ef0bbd821706e29b123/unix-i586/jre-8u331-macosx-x64.dmg"
+
+# Java 10 Runtime
+url_java10="https://javadl.oracle.com/webapps/download/GetFile/10.0.2+13/19aef61b38124481863b1413dce1855f/unix-i586/jre-10.0.2_osx-x64_bin.dmg"
+
+# Mozilla Firefox
+url_firefox="https://download.mozilla.org/?product=firefox-latest-ssl&os=osx"
+
+# Mozilla Thunderbird
+url_thunderbird="https://download.mozilla.org/?product=thunderbird-latest-ssl&os=osx"
+
+# Zoom
+url_zoom="https://zoom.us/client/latest/Zoom.pkg"
+
+# Microsoft Office
+url_microsoft_office="https://go.microsoft.com/fwlink/p/?linkid=525133"
+url_microsoft_office_2019_1643="https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_16.43.20110804_Installer.pkg"
+url_microsoft_office_2016="https://go.microsoft.com/fwlink/?linkid=871743"
+url_microsoft_office_2011=""
+
+# Dropbox
+url_dropbox="https://www.dropbox.com/download?plat=mac&full=1"
+
+
