5d778b43c063cd7f66b9186adeba93a4689f9ee7
tedzhangzg <83065920+tedzhangzg@users.noreply.github.com>
2022-05-10 10:21:30 +0800

    Create urls.ps1

---
+++ urls.ps1
@@ -0,0 +1,63 @@
+# Script
+# URL References
+
+# To download and include
+# Invoke-Expression $($(Invoke-WebRequest "https://raw.githubusercontent.com/tedzhangzg/shellscripts/main/urls.ps1" -UseBasicParsing).Content)
+
+
+
+# C++ Runtime framework packages for Desktop Bridge
+$url_cppdb14x86 = "https://aka.ms/Microsoft.VCLibs.x86.14.00.Desktop.appx"
+$url_cppdb14x64 = "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
+
+# Windows Package Manager
+$url_winget = "https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+# Old Version
+# $url_winget = "https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+# $url_winget = "https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+# $url_winget = "https://github.com/microsoft/winget-cli/releases/download/v1.0.11451/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+
+# Microsoft .NET Framework 1.1
+$url_dotnetfw11rtm = "https://raw.githubusercontent.com/not001praween001/stockMN-exe/master/stockMN_1.3.1/dotnetfx.exe"
+$url_dotnetfw11sp1 = "https://download.microsoft.com/download/8/b/4/8b4addd8-e957-4dea-bdb8-c4e00af5b94b/NDP1.1sp1-KB867460-X86.exe"
+
+# Visual C++ Redistributable x86
+$url_vcpp2013x86 = "https://aka.ms/highdpimfc2013x86enu"
+$url_vcppLatestx86 = "https://aka.ms/vs/17/release/vc_redist.x86.exe"
+
+# C++ Runtime v11.0 framework package for Desktop Bridge (Project Centennial)
+$url_cppdb11x86 = "https://download.microsoft.com/download/3/B/C/3BC60F47-6A7C-4D46-8CFB-C2E746EF336E/vc_uwpdesktop.110.exe"
+
+# C++ Runtime v12 framework package for Desktop Bridge (Project Centennial)
+$url_cppdb12x86 = "https://download.microsoft.com/download/2/1/F/21F05B77-C444-46C0-B357-7EBC78C95CE2/vc_uwpdesktop.120.exe"
+
+# Linux kernel update package x64
+$url_wsl2kernel = "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
+
+# Windows Essentials 2012
+$url_winessen = "https://archive.org/download/wlsetup-all_201802/wlsetup-all.exe"
+
+# Skype MSI
+$url_skype = "http://go.skype.com/msi-download"
+# When using curl, rename file to Skype-_ver_.msi
+
+# Google Drive
+$url_googledrive = "https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe"
+
+# TeamViewer QuickSupport v13
+$url_teamviewer = "https://download.teamviewer.com/download/version_13x/TeamViewerQS.exe"
+
+# Java 8 Runtime x86
+$url_java8x86 = "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_331-b09/165374ff4ea84ef0bbd821706e29b123/windows-i586/jre-8u331-windows-i586.exe"
+
+# Java 10 Runtime
+$url_java10 = "https://javadl.oracle.com/webapps/download/GetFile/10.0.2+13/19aef61b38124481863b1413dce1855f/windows-i586/jre-10.0.2_windows-x64_bin.exe"
+
+# Office 2013 ODT
+$url_odt2013 = "https://download.microsoft.com/download/6/2/3/6230F7A2-D8A9-478B-AC5C-57091B632FCF/officedeploymenttool_x86_5031-1000.exe"
+
+
+
+# Appendix
+
+
