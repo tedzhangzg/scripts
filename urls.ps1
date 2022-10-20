87534723718e7d212844245b6a7d79835d7dd905
tedzhangzg <83065920+tedzhangzg@users.noreply.github.com>
2022-10-20 13:30:48 +0800

    Update urls.ps1

--- urls.ps1
+++ urls.ps1
@@ -80,7 +80,7 @@ $url_googledrive = "https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe
 $url_teamviewer = "https://download.teamviewer.com/download/version_13x/TeamViewerQS.exe"
 
 # Java 8 JRE x86
-$url_java8_x86 = "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_341-b10/424b9da4b48848379167015dcc250d8d/windows-i586/jre-8u341-windows-i586.exe"
+$url_java8_x86 = "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_351-b10/10e8cce67c7843478f41411b7003171c/windows-i586/jre-8u351-windows-i586.exe"
 
 # Java 10 JRE
 $url_java10 = "https://javadl.oracle.com/webapps/download/GetFile/10.0.2+13/19aef61b38124481863b1413dce1855f/windows-i586/jre-10.0.2_windows-x64_bin.exe"
