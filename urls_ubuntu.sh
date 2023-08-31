fd75da96e12269f65e9416d155aec296bf341769
tedzhangzg <83065920+tedzhangzg@users.noreply.github.com>
2023-08-31 17:06:44 +0800

    Update urls_ubuntu.sh

--- urls_ubuntu.sh
+++ urls_ubuntu.sh
@@ -25,12 +25,8 @@ url_javajdk_8_x86="https://javadl.oracle.com/webapps/download/GetFile/1.8.0_381-
 url_javajdk_10_x64="https://javadl.oracle.com/webapps/download/GetFile/10.0.2+13/19aef61b38124481863b1413dce1855f/linux-i586/jdk-10.0.2_linux-x64_bin.tar.gz"
 # url_javajdk_11_a64="https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java11/java11020/jdk-11.0.20_linux-aarch64_bin.tar.gz"
 url_javajdk_11_x64="https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java11/java11020/jdk-11.0.20_linux-x64_bin.tar.gz"
-
-url_javajdk_17_a64="https://download.oracle.com/java/17/archive/jdk-17.0.6_macos-aarch64_bin.dmg"
-url_javajdk_17_x64="https://download.oracle.com/java/17/archive/jdk-17.0.6_macos-x64_bin.dmg"
-
-
-
+url_javajdk_17_a64="https://download.oracle.com/java/17/archive/jdk-17.0.8_linux-aarch64_bin.tar.gz"
+url_javajdk_17_x64="https://download.oracle.com/java/17/archive/jdk-17.0.8_linux-x64_bin.tar.gz"
 
 
 
