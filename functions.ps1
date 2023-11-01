c53df245ebee4bfd2db73d794088769e19e11c5f
tedzhangzg <83065920+tedzhangzg@users.noreply.github.com>
2023-11-01 15:36:37 +0800

    Update functions.ps1

--- functions.ps1
+++ functions.ps1
@@ -139,61 +139,61 @@ Function Is-Win11() {
 # 
 # Versions
 # 
-# Windows Server 2022: 10, 0, 20348
-# Windows Server 2019: 10, 0, 17763
-# Windows Server 2016: 10, 0, 14393
-# Windows Server 2012 R2: 6, 3, 9600
-# Windows Server 2012: 6, 2, 9200
-# Windows Server 2008 R2 SP1: 6, 1, 7601
-# Windows Server 2008 R2: 6, 1, 7600
-# Windows Server 2008 SP2 Update Rollup: 6, 0, 6003
-# Windows Server 2008 SP2: 6, 0, 6002
-# Windows Server 2008: 6, 0, 6001
-# Windows Server 2003 R2: 5, 2, 3790
-# Windows Server 2003 SP2: 5, 2, 3790
-# Windows Server 2000: 5, 0, 2195
+# Windows Server 2022: 10.0.20348
+# Windows Server 2019: 10.0.17763
+# Windows Server 2016: 10.0.14393
+# Windows Server 2012 R2: 6.3.9600
+# Windows Server 2012: 6.2.9200
+# Windows Server 2008 R2 SP1: 6.1.7601
+# Windows Server 2008 R2: 6.1.7600
+# Windows Server 2008 SP2 Update Rollup: 6.0.6003
+# Windows Server 2008 SP2: 6.0.6002
+# Windows Server 2008: 6.0.6001
+# Windows Server 2003 R2: 5.2.3790
+# Windows Server 2003 SP2: 5.2.3790
+# Windows Server 2000: 5.0.2195
 # 
-# Windows Server version 23H2: 10, 0, ?
-# Windows Server version 20H2: 10, 0, ?
-# Windows Server version 2004: 10, 0, ?
-# Windows Server version 1909: 10, 0, ?
-# Windows Server version 1903: 10, 0, ?
-# Windows Server version 1809: 10, 0, ?
-# Windows Server version 1803: 10, 0, ?
-# Windows Server version 1709: 10, 0, ?
+# Windows Server version 23H2: 10.0.?
+# Windows Server version 20H2: 10.0.?
+# Windows Server version 2004: 10.0.?
+# Windows Server version 1909: 10.0.?
+# Windows Server version 1903: 10.0.?
+# Windows Server version 1809: 10.0.?
+# Windows Server version 1803: 10.0.?
+# Windows Server version 1709: 10.0.?
 # 
-# Windows 11 23H2: 10, 0, 22631
-# Windows 11 22H2: 10, 0, 22621
-# Windows 11 21H2: 10, 0, 22000
+# Windows 11 23H2: 10.0.22631
+# Windows 11 22H2: 10.0.22621
+# Windows 11 21H2: 10.0.22000
 # 
-# Windows 10 22H2: 10, 0, 19045
-# Windows 10 21H2: 10, 0, 19044
-# Windows 10 21H1: 10, 0, 19043
-# Windows 10 20H2: 10, 0, 19042
-# Windows 10 2004: 10, 0, 19041
-# Windows 10 1909: 10, 0, 18363
-# Windows 10 1903: 10, 0, 18362
-# Windows 10 1809: 10, 0, 17763
-# Windows 10 1803: 10, 0, 17134
-# Windows 10 1709: 10, 0, 16299
-# Windows 10 1703: 10, 0, 15063
-# Windows 10 1607: 10, 0, 14393
-# Windows 10 1511: 10, 0, 10586
-# Windows 10 1507: 10, 0, 10240
+# Windows 10 22H2: 10.0.19045
+# Windows 10 21H2: 10.0.19044
+# Windows 10 21H1: 10.0.19043
+# Windows 10 20H2: 10.0.19042
+# Windows 10 2004: 10.0.19041
+# Windows 10 1909: 10.0.18363
+# Windows 10 1903: 10.0.18362
+# Windows 10 1809: 10.0.17763
+# Windows 10 1803: 10.0.17134
+# Windows 10 1709: 10.0.16299
+# Windows 10 1703: 10.0.15063
+# Windows 10 1607: 10.0.14393
+# Windows 10 1511: 10.0.10586
+# Windows 10 1507: 10.0.10240
 # 
-# Windows 8.1 Update: 6, 3, 9600
-# Windows 8.1: 6, 3, 9200
-# Windows 8: 6, 2, 9200
+# Windows 8.1 Update: 6.3.9600
+# Windows 8.1: 6.3.9200
+# Windows 8: 6.2.9200
 # 
-# Windows 7 SP1: 6, 1, 7601
-# Windows 7: 6, 1, 7600
+# Windows 7 SP1: 6.1.7601
+# Windows 7: 6.1.7600
 # 
-# Windows Vista SP2: 6, 0, 6002
-# Windows Vista SP1: 6, 0, 6001
-# Windows Vista: 6, 0, 6000
+# Windows Vista SP2: 6.0.6002
+# Windows Vista SP1: 6.0.6001
+# Windows Vista: 6.0.6000
 # 
 # Windows XP SP3: 5, 1, 2600
-# Windows 2000 SP4: 5, 0, 2195
+# Windows 2000 SP4: 5.0.2195
 # 
 # Check if Server or Desktop
 # (Get-CimInstance -ClassName "Win32_OperatingSystem").ProductType
