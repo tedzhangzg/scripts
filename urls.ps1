da7867131627553bf572d16ceaf8a54c586926ba
tedzhangzg <83065920+tedzhangzg@users.noreply.github.com>
2024-10-28 07:56:03 +0800

    Update urls.ps1

--- urls.ps1
+++ urls.ps1
@@ -84,8 +84,8 @@ $url_googledrive = "https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe
 $url_teamviewerqs_13 = "https://download.teamviewer.com/download/version_13x/TeamViewerQS.exe"
 
 # Java JRE
-$url_javajre_8_x64 = "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_421-b09/d8aa705069af427f9b83e66b34f5e380/windows-i586/jre-8u421-windows-x64.exe"
-$url_javajre_8_x86 = "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_421-b09/d8aa705069af427f9b83e66b34f5e380/windows-i586/jre-8u421-windows-i586.exe"
+$url_javajre_8_x64 = "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_431-b10/0d8f12bc927a4e2c9f8568ca567db4ee/windows-i586/jre-8u431-windows-x64.exe"
+$url_javajre_8_x86 = "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_431-b10/0d8f12bc927a4e2c9f8568ca567db4ee/windows-i586/jre-8u431-windows-i586.exe"
 $url_javajre_10_x64 = "https://javadl.oracle.com/webapps/download/GetFile/10.0.2+13/19aef61b38124481863b1413dce1855f/windows-i586/jre-10.0.2_windows-x64_bin.exe"
 
 # Java JDK
