bd5f0270e514540fc4d15d4cd428746485ff021d
tedzhangzg <83065920+tedzhangzg@users.noreply.github.com>
2025-04-12 22:34:39 +0800

    Update urls.sh

--- urls.sh
+++ urls.sh
@@ -27,9 +27,6 @@ url_microsoft_edge="https://go.microsoft.com/fwlink/?linkid=2069148"
 # Microsoft Teams
 url_microsoft_teams="https://aka.ms/teamsmac"
 
-# Microsoft Skype
-url_skype="https://go.skype.com/mac.download"
-
 # Google Chrome
 url_google_chrome="https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
 url_google_chrome_pkg="https://dl.google.com/dl/chrome/mac/universal/stable/gcem/GoogleChrome.pkg"
@@ -46,19 +43,13 @@ url_zoom="https://zoom.us/client/latest/Zoom.pkg"
 # TeamViewer QuickSupport v13
 url_teamviewerqs_13="https://download.teamviewer.com/download/version_13x/TeamViewerQS.dmg"
 
-# Java JRE
+# Java JRE JDK
 url_javajre_8_a64="https://javadl.oracle.com/webapps/download/GetFile/1.8.0_441-b07/7ed26d28139143f38c58992680c214a5/unix-i586/jre-8u441-macosx-aarch64.dmg"
 url_javajre_8_x64="https://javadl.oracle.com/webapps/download/GetFile/1.8.0_441-b07/7ed26d28139143f38c58992680c214a5/unix-i586/jre-8u441-macosx-x64.dmg"
 url_javajre_10_x64="https://javadl.oracle.com/webapps/download/GetFile/10.0.2+13/19aef61b38124481863b1413dce1855f/unix-i586/jre-10.0.2_osx-x64_bin.dmg"
-
-# Java JDK
 # url_javajdk_8_a64="https://javadl.oracle.com/webapps/download/GetFile/1.8.0_381-b09/8c876547113c4e4aab3c868e9e0ec572/unix-i586/jdk-8u381-macosx-aarch64.dmg"
 url_javajdk_8_x64="https://javadl.oracle.com/webapps/download/GetFile/1.8.0_381-b09/8c876547113c4e4aab3c868e9e0ec572/unix-i586/jdk-8u381-macosx-x64.dmg"
 url_javajdk_10_x64="https://javadl.oracle.com/webapps/download/GetFile/10.0.2+13/19aef61b38124481863b1413dce1855f/unix-i586/jdk-10.0.2_osx-x64_bin.dmg"
-url_javajdk_11_a64="https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java11/java11020/jdk-11.0.20_macos-aarch64_bin.dmg"
-url_javajdk_11_x64="https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java11/java11020/jdk-11.0.20_macos-x64_bin.dmg"
-url_javajdk_17_a64="https://download.oracle.com/java/17/archive/jdk-17.0.8_macos-aarch64_bin.dmg"
-url_javajdk_17_x64="https://download.oracle.com/java/17/archive/jdk-17.0.8_macos-x64_bin.dmg"
 
 # Mozilla Firefox
 url_firefox="https://download.mozilla.org/?product=firefox-latest-ssl&os=osx"
@@ -69,41 +60,41 @@ url_thunderbird="https://download.mozilla.org/?product=thunderbird-latest-ssl&os
 # Microsoft Office
 # 
 # latest version
-# ver 16
+# v16
 url_microsoft_office="https://go.microsoft.com/fwlink/p/?linkid=525133"
 # for Office with Teams
 # url_microsoft_office="https://go.microsoft.com/fwlink/p/?linkid=2009112"
 # 
 # last version for macOS 13
-# ver 2024
+# v2024
 url_microsoft_office_16_v16xx="https://go.microsoft.com/fwlink/p/?linkid=525133"
 # 
 # last version for macOS 12
-# ver 2021
+# v2021
 url_microsoft_office_16_v1689="https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_365_and_Office_16.89.24091630_Installer.pkg"
 # 
 # note
 # last version for Office 2019 license is v16.78
 # 
 # last version for macOS 11
-# ver 2019v2 2021
+# v2019v2 or v2021
 url_microsoft_office_16_v1677="https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_365_and_Office_16.77.23091003_Installer.pkg"
 # 
 # last version for macOS 10.15
-# ver 2019v2 2021
+# v2019v2 or v2021
 url_microsoft_office_16_v1666="https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_16.66.22101101_Installer.pkg"
 # url_microsoft_office_16_v1666="https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_16.66.22100900_Installer.pkg"
 # 
 # last version for macOS 10.14
-# ver 2019v1 2021
+# v2019v1 or v2021
 url_microsoft_office_16_v1654="https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_16.54.21101001_Installer.pkg"
 # 
 # last version for macOS 10.13
-# ver 2019v1
+# v2019v1
 url_microsoft_office_2019_v1643="https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_16.43.20110804_Installer.pkg"
 # 
 # last version for macOS 10.12
-# ver 2019v1
+# v2019v1
 url_microsoft_office_2019_v1629="https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_16.29.19090802_Installer.pkg"
 # 
 # note
@@ -114,7 +105,7 @@ url_microsoft_office_2019_v1629="https://officecdn.microsoft.com/pr/C1297A47-86C
 # last version for Office 2016 license is v16.16
 # 
 # last version for macOS 10.11
-# ver 2016
+# v2016
 url_microsoft_office_2016_v1616="https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_16.16.20101200_Installer.pkg"
 # url_microsoft_office_2016_v1616="https://go.microsoft.com/fwlink/?linkid=871743"
 # 
@@ -128,3 +119,6 @@ url_microsoft_office_2011=""
 # url_office2019="https://go.microsoft.com/fwlink/?linkid=525133"
 # url_office2016="https://go.microsoft.com/fwlink/?linkid=871743"
 
+
+
+# Appendix
