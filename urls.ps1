10c44c42a4c6bf04a4ca415a4dd35e7482136842
tedzhangzg <83065920+tedzhangzg@users.noreply.github.com>
2025-04-12 22:15:25 +0800

    Update urls.ps1

--- urls.ps1
+++ urls.ps1
@@ -66,10 +66,10 @@ $url_vcpp_2015Plus_a64 = "https://aka.ms/vs/17/release/vc_redist.arm64.exe"
 $url_vcpp_2015Plus_x64 = "https://aka.ms/vs/17/release/vc_redist.x64.exe"
 $url_vcpp_2015Plus_x86 = "https://aka.ms/vs/17/release/vc_redist.x86.exe"
 
-# C++ Runtime v11.0 framework package for Desktop Bridge (Project Centennial)
+# C++ Runtime v11.0 framework package for Desktop Bridge
 $url_cppdb_11_x86 = "https://download.microsoft.com/download/3/B/C/3BC60F47-6A7C-4D46-8CFB-C2E746EF336E/vc_uwpdesktop.110.exe"
 
-# C++ Runtime v12 framework package for Desktop Bridge (Project Centennial)
+# C++ Runtime v12 framework package for Desktop Bridge
 $url_cppdb_12_x86 = "https://download.microsoft.com/download/2/1/F/21F05B77-C444-46C0-B357-7EBC78C95CE2/vc_uwpdesktop.120.exe"
 
 # Linux kernel update package x64
@@ -84,26 +84,16 @@ $url_vscode_x86 = "https://code.visualstudio.com/sha/download?build=stable&os=wi
 # Windows Essentials 2012
 $url_winessentials_2012 = "https://archive.org/download/wlsetup-all_201802/wlsetup-all.exe"
 
-# Skype MSI
-$url_skypemsi = "http://go.skype.com/msi-download"
-
-# Google Drive
-$url_googledrive = "https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe"
-
 # TeamViewer QuickSupport v13
 $url_teamviewerqs_13 = "https://download.teamviewer.com/download/version_13x/TeamViewerQS.exe"
 
-# Java JRE
+# Java JRE JDK
 $url_javajre_8_x64 = "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_431-b10/0d8f12bc927a4e2c9f8568ca567db4ee/windows-i586/jre-8u431-windows-x64.exe"
 $url_javajre_8_x86 = "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_431-b10/0d8f12bc927a4e2c9f8568ca567db4ee/windows-i586/jre-8u431-windows-i586.exe"
 $url_javajre_10_x64 = "https://javadl.oracle.com/webapps/download/GetFile/10.0.2+13/19aef61b38124481863b1413dce1855f/windows-i586/jre-10.0.2_windows-x64_bin.exe"
-
-# Java JDK
 $url_javajdk_8_x64 = "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_381-b09/8c876547113c4e4aab3c868e9e0ec572/windows-i586/jdk-8u381-windows-x64.exe"
 $url_javajdk_8_x86 = "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_381-b09/8c876547113c4e4aab3c868e9e0ec572/windows-i586/jdk-8u381-windows-i586.exe"
 $url_javajdk_10_x64 = "https://javadl.oracle.com/webapps/download/GetFile/10.0.2+13/19aef61b38124481863b1413dce1855f/windows-i586/jdk-10.0.2_windows-x64_bin.exe"
-$url_java_11_x64 = "https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java11/java11020/jdk-11.0.20_windows-x64_bin.exe"
-$url_java_17_x64 = "https://download.oracle.com/java/17/archive/jdk-17.0.8_windows-x64_bin.exe"
 
 # Office 2013 ODT
 $url_odt_2013 = "https://download.microsoft.com/download/6/2/3/6230F7A2-D8A9-478B-AC5C-57091B632FCF/officedeploymenttool_x86_5031-1000.exe"
@@ -115,7 +105,7 @@ $url_msaccessdbengine_2016_x64 = "https://download.microsoft.com/download/3/5/C/
 $url_msaccessdbengine_2016_x86 = "https://download.microsoft.com/download/3/5/C/35C84C36-661A-44E6-9324-8786B8DBE231/accessdatabaseengine.exe"
 
 # KMS
-$url_micactscr = "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd"
+$url_msactscript = "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd"
 $url_convr2v = "https://raw.githubusercontent.com/abbodi1406/C2R-R2V-AIO/master/C2R-R2V-AIO.cmd"
 
 
